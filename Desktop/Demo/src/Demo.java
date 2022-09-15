d4b3b96c7a17d0f0a3e015ce2e0e8e97cc725689
Jonathan Varela <jonvarela4@gmail.com>
2022-09-15 16:50:21 -0400

    Complete

---
+++ Desktop/Demo/src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static String noun1 = "thimble";
+    public String adjective = "cheesy";
+    public String noun2 = "football";
+    public String verb = "slice";
+    public String noun3 = "rain";
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello bro.");
+    }
+}

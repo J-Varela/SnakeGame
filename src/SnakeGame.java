b734fe009751d18166586b4aef7d7d4abda520e1
Jonathan Varela <jonvarela4@gmail.com>
2022-11-09 06:23:12 -0500

    Updated Snake.java

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,7 +3,7 @@ public class SnakeGame {
     public static void main(String[] args) {
         //create an instance of our
         new GameFrame(); // Don't plan on using this frame for anything else.
-        System.out.println("Hello there");
+
 
     }
 }
